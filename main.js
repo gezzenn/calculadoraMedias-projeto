0ea1ac3fc002b3178fe7ed5a876022761588c932
gezenn <grillogm11@gmail.com>
2025-08-20 15:07:21 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -48,8 +48,8 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calcularMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal.toFixed(2) >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calcularMediaFinal() {
